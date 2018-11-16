1cdf025f1c11251852d31335db70717ae6b0ad6e
BraginDmirty <dimok.bratok443@mail.ru>
2018-11-16 21:01:17 +0300

    работа с первой таблицей, дальше пойдет быстрее, надеюсь

---
+++ TP/TP/DAO/GroupDogDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TP.Models;
+
+namespace TP.DAO
+{
+    public class GroupDogDAO
+    {
+        private tpEntities1 _entities = new tpEntities1();
+        public IEnumerable<GroupDog> GetAllGroups()
+        {
+            return (from c in _entities.GroupDog
+                    select c);
+        }
+    }
+}
